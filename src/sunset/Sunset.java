c8553a053cd93d56ea0168142708274b709a303a
Slish <81778048+Slish563@users.noreply.github.com>
2021-05-09 19:22:51 +0300

    Update

--- src/sunset/Sunset.java
+++ src/sunset/Sunset.java
@@ -1,17 +1,36 @@
 package sunset;
 
+import arc.*;
+import arc.util.*;
+import mindustry.game.EventType.*;
+import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
+import sunset.content.*;
 import arc.util.Log;
-import sunset.content.Snblocks;
-import sunset.content.Snitems;
-import mindustry.ctype.ContentList;
-import mindustry.mod.Mod;
+import mindustry.ctype.*;
 
 public class Sunset extends Mod{
 
     private final ContentList[] sunsetContent = {
       new Snitems(),
-      new Snblocks()
+      new Snblocks(),
     };
+         public SunsetJavaMod(){
+        Log.info("Loaded ExampleJavaMod constructor.");
+
+        //listen for game load event
+        Events.on(ClientLoadEvent.class, e -> {
+            //show dialog upon startup
+            Time.runTask(10f, () -> {
+                BaseDialog dialog = new BaseDialog("hello");
+                dialog.cont.add("behold").row();
+                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
+                dialog.cont.image(Core.atlas.find("sunset-java-mod-logo")).pad(20f).row();
+                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
+                dialog.show();
+            });
+        });
+    }
 
     @Override
     public void loadContent(){
