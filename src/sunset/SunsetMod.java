b67a0fa635c2274309b3ef77ea3fdff6c1cd868a
Slish <81778048+Slish563@users.noreply.github.com>
2021-05-13 17:46:41 +0300

    Add files via upload

---
+++ src/sunset/SunsetMod.java
@@ -0,0 +1,39 @@
+package sunset;
+
+import arc.*;
+import arc.util.*;
+import mindustry.game.EventType.*;
+import mindustry.mod.*;
+import sunset.content.*;
+import arc.util.Log;
+import mindustry.ctype.*;
+
+    public class NewHorizon extends Mod{
+	public static final String MOD_NAME = "sunset";
+	public static Links.LinkEntry[] links;
+	
+	public static String configName(String name){
+		return MOD_NAME + name;
+        
+	}
+
+    private static final ContentList[] content = {
+    new Snitems(),
+    new Snblocks(),
+
+    };
+
+    @Override
+    public void init(){
+        
+    }
+    
+    @Override
+    public void loadContent(){
+        for(ContentList list : sunsetContent){
+            list.load();
+
+            Log.info("@: Loaded content list: @", getClass().getSimpleName(), list.getClass().getSimpleName());
+        }
+    }
+}
