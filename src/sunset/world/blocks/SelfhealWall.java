dbe980e2fb62a6c8dbf5c3cff6998c3026597c0c
KirillAldashkin <a89086520092k@yandex.ru>
2021-06-10 15:50:27 +0800

    Naturit wall now selfheals, health rebalance

---
+++ src/sunset/world/blocks/SelfhealWall.java
@@ -0,0 +1,29 @@
+package sunset.world.blocks;
+
+import mindustry.world.blocks.defense.Wall;
+import mindustry.world.meta.Stat;
+import mindustry.world.meta.StatUnit;
+
+/** Самовосстанавливающаяся стена. */
+public class SelfhealWall extends Wall {
+    public float heal = 0.1f;
+
+    public SelfhealWall(String name) {
+        super(name);
+        update = true;
+    }
+
+    @Override
+    public void setStats() {
+        super.setStats();
+        stats.add(Stat.repairTime, health / heal / 60f, StatUnit.seconds);
+    }
+
+    public class SelfhealWallBuild extends WallBuild {
+        @Override
+        public void updateTile() {
+            super.updateTile();
+            heal(heal * delta());
+        }
+    }
+}
