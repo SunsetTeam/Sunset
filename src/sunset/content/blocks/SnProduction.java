6fc77ed16b95e4e24920f7f113c4643faa561f7e
Slish <81778048+Slish563@users.noreply.github.com>
2021-06-14 16:47:00 +0300

    Sorting

--- src/sunset/content/blocks/SnProduction.java
+++ src/sunset/content/blocks/SnProduction.java
@@ -26,86 +26,16 @@ import sunset.world.draw.DrawWeaveTop;
 public class SnProduction implements ContentList{
     public static Block
 
-  //standart
-    crystalyze, purifier, enojieKiln,
-
     //advanced
-    advancedSurge, advancedCompressor, advancedKiln, advancedWeaver, advancedCultivator;
+    advancedCompressor, advancedWeaver, advancedCultivator, advancedKiln, advancedSurge,
+
+    //standart
+    crystalyze, purifier, enojieKiln;
 
   @Override
   public void load(){
 
-  //standart
-    crystalyze = new GenericSmelter("crystalyze"){{
-        requirements(Category.crafting, with(Items.lead, 130, Items.thorium, 110, Items.silicon, 120, SnItems.nobium, 90, Items.plastanium, 75));
-
-        craftEffect = SnFx.crystalyze;
-        updateEffect = SnFx.crystalyzeSmall;
-        hasItems = true;
-        itemCapacity = 30;
-        liquidCapacity = 30f;
-        outputItem = new ItemStack(SnItems.naturite, 2);
-        craftTime = 34f;
-        size = 4;
-        hasPower = hasItems = true;
-        flameColor = Color.valueOf("F9ECA3");
-
-        consumes.item(Items.sand, 4);
-        consumes.liquid(Liquids.water, 0.20f);
-        consumes.power(2.7f);
-    }};
-
-    purifier = new GenericCrafter("purifier"){{
-        requirements(Category.crafting, with(Items.copper, 120, Items.titanium, 95, Items.silicon, 80, Items.plastanium, 65));
-
-        outputItem = new ItemStack(SnItems.nobium, 2);
-        craftEffect = Fx.pulverize;
-        updateEffect = Fx.pulverizeSmall;
-        craftTime = 25f;
-        size = 3;
-        hasPower = true;
-        hasLiquids = false;
-        drawer = new DrawRotator();
-        ambientSound = Sounds.grinding;
-        ambientSoundVolume = 0.025f;
-
-        consumes.items(with(Items.titanium, 3, Items.thorium, 2, SnItems.fors, 1));
-        consumes.power(2.5f);
-    }};
-
-
-    enojieKiln = new GenericCrafter("enojie-kiln"){{
-        requirements(Category.crafting, with(Items.lead, 200, SnItems.nobium, 150, Items.graphite, 140, Items.silicon, 120, Items.surgeAlloy, 80));
-
-       outputItem = new ItemStack(SnItems.enojie, 1);
-       craftTime = 36f;
-       size = 4;
-       hasPower = true;
-       hasLiquids = false;
-       craftEffect = SnFx.enojiecraft;
-       updateEffect = SnFx.enojieburn;
-
-       consumes.items(with(SnItems.nobium, 2, SnItems.planatrium, 3, Items.metaglass, 1));
-       consumes.power(5.3f);
-    }};
-
     //advanced
-    advancedSurge = new StaticTopSmelter("advancedSurge"){{
-        requirements(Category.crafting, with(Items.lead, 130, Items.metaglass, 100, Items.silicon, 150, Items.thorium, 110, Items.surgeAlloy, 75, SnItems.naturite, 75));
-
-        size = 5;
-        craftEffect = SnFx.modSmokeCloud;
-        craftTime = 90f;
-        outputItem = new ItemStack(Items.surgeAlloy, 7);
-        itemCapacity = 50;
-        liquidCapacity = 80f;
-        drawer = new Draw360Rotator();
-
-        consumes.items(with(Items.copper, 10, Items.lead, 12, Items.titanium, 8, Items.silicon, 10, Items.pyratite, 3));
-        consumes.liquid(Liquids.cryofluid, 0.9f);
-        consumes.power(5.5f);
-    }};
-
     advancedCompressor = new GenericSmelter("advancedCompressor"){{
         requirements(Category.crafting, with(Items.lead, 150, Items.graphite, 120, Items.silicon, 210, Items.titanium, 110, Items.thorium, 200, Items.phaseFabric, 110, SnItems.naturite, 95));
 
@@ -127,24 +57,7 @@ public class SnProduction implements ContentList{
         consumes.liquid(Liquids.oil, 0.6f);
         consumes.power(5.3f);
     }};
-
-    advancedKiln = new GenericSmelter("advancedKiln"){{
-        requirements(Category.crafting, with(Items.copper, 210, Items.lead, 220, Items.metaglass, 190, Items.graphite, 185, Items.silicon, 200, Items.titanium, 210, Items.thorium, 205, SnItems.flameid, 95));
-
-        size = 4;
-        health = 1200;
-        craftEffect = SnFx.modSmokeCloud;
-        craftTime = 95f;
-        outputItem = new ItemStack(Items.metaglass, 9);
-        drawer = new DrawRotatorTop();
-        itemCapacity = 15;
-        liquidCapacity = 100f;
-
-        consumes.items(with(Items.lead, 7, Items.sand, 5));
-        consumes.liquid(Liquids.water, 1f);
-        consumes.power(7f);
-    }};
-
+   
     advancedWeaver = new GenericSmelter("advancedWeaver"){{
         requirements(Category.crafting, with(Items.copper, 210, Items.metaglass, 90, Items.silicon, 190, Items.titanium, 100, Items.thorium, 185, Items.surgeAlloy, 110, SnItems.flameid, 100));
         size = 3;
@@ -177,6 +90,93 @@ public class SnProduction implements ContentList{
 
         consumes.liquid(Liquids.water, 0.7f);
         consumes.power(2f);
+    }};
+    
+    advancedKiln = new GenericSmelter("advancedKiln"){{
+        requirements(Category.crafting, with(Items.copper, 210, Items.lead, 220, Items.metaglass, 190, Items.graphite, 185, Items.silicon, 200, Items.titanium, 210, Items.thorium, 205, SnItems.flameid, 95));
+
+        size = 4;
+        health = 1200;
+        craftEffect = SnFx.modSmokeCloud;
+        craftTime = 95f;
+        outputItem = new ItemStack(Items.metaglass, 9);
+        drawer = new DrawRotatorTop();
+        itemCapacity = 15;
+        liquidCapacity = 100f;
+
+        consumes.items(with(Items.lead, 7, Items.sand, 5));
+        consumes.liquid(Liquids.water, 1f);
+        consumes.power(7f);
+    }};
+
+
+    advancedSurge = new StaticTopSmelter("advancedSurge"){{
+        requirements(Category.crafting, with(Items.lead, 130, Items.metaglass, 100, Items.silicon, 150, Items.thorium, 110, Items.surgeAlloy, 75, SnItems.naturite, 75));
+
+        size = 5;
+        craftEffect = SnFx.modSmokeCloud;
+        craftTime = 90f;
+        outputItem = new ItemStack(Items.surgeAlloy, 7);
+        itemCapacity = 50;
+        liquidCapacity = 80f;
+        drawer = new Draw360Rotator();
+
+        consumes.items(with(Items.copper, 10, Items.lead, 12, Items.titanium, 8, Items.silicon, 10, Items.pyratite, 3));
+        consumes.liquid(Liquids.cryofluid, 0.9f);
+        consumes.power(5.5f);
+    }};
+    
+//standart
+    purifier = new GenericCrafter("purifier"){{
+        requirements(Category.crafting, with(Items.copper, 120, Items.titanium, 95, Items.silicon, 80, Items.plastanium, 65));
+
+        outputItem = new ItemStack(SnItems.nobium, 2);
+        craftEffect = Fx.pulverize;
+        updateEffect = Fx.pulverizeSmall;
+        craftTime = 25f;
+        size = 3;
+        hasPower = true;
+        hasLiquids = false;
+        drawer = new DrawRotator();
+        ambientSound = Sounds.grinding;
+        ambientSoundVolume = 0.025f;
+
+        consumes.items(with(Items.titanium, 3, Items.thorium, 2, SnItems.fors, 1));
+        consumes.power(2.5f);
+    }};
+
+    crystalyze = new GenericSmelter("crystalyze"){{
+        requirements(Category.crafting, with(Items.lead, 130, Items.thorium, 110, Items.silicon, 120, SnItems.nobium, 90, Items.plastanium, 75));
+
+        craftEffect = SnFx.crystalyze;
+        updateEffect = SnFx.crystalyzeSmall;
+        hasItems = true;
+        itemCapacity = 30;
+        liquidCapacity = 30f;
+        outputItem = new ItemStack(SnItems.naturite, 2);
+        craftTime = 34f;
+        size = 4;
+        hasPower = hasItems = true;
+        flameColor = Color.valueOf("F9ECA3");
+
+        consumes.item(Items.sand, 4);
+        consumes.liquid(Liquids.water, 0.20f);
+        consumes.power(2.7f);
+    }};
+
+    enojieKiln = new GenericCrafter("enojie-kiln"){{
+        requirements(Category.crafting, with(Items.lead, 200, SnItems.nobium, 150, Items.graphite, 140, Items.silicon, 120, Items.surgeAlloy, 80));
+
+       outputItem = new ItemStack(SnItems.enojie, 1);
+       craftTime = 36f;
+       size = 4;
+       hasPower = true;
+       hasLiquids = false;
+       craftEffect = SnFx.enojiecraft;
+       updateEffect = SnFx.enojieburn;
+
+       consumes.items(with(SnItems.nobium, 2, SnItems.planatrium, 3, Items.metaglass, 1));
+       consumes.power(5.3f);
     }};
   }
 }
