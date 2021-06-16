f2a4142db2e2fef6007a53b52e87808371eff83a
Slish <81778048+Slish563@users.noreply.github.com>
2021-06-16 17:30:18 +0300

    Woops

--- src/sunset/content/SnSounds.java
+++ src/sunset/content/SnSounds.java
@@ -7,27 +7,24 @@ import arc.audio.Sound;
 import mindustry.Vars;
 
 public class SnSounds {
-    protected static Sound loadSound(String soundName) {
-        if(!Vars.headless) {
-            String name = "sounds/" + soundName;
-            String path = Vars.tree.get(name + ".ogg").exists() ? name + ".ogg" : name + ".mp3";
-    
-            Sound sound = new Sound();
-    
-            AssetDescriptor<?> desc = Core.assets.load(path, Sound.class, new SoundLoader.SoundParameter(sound));
-            desc.errored = Throwable::printStackTrace;
-    
-            return sound;
-        } else {
-            return new Sound();
-        }
-    }
+
     public static Sound
-        blizzardWind;
+        blizzardWind = new Sound();
 
     public static void load() {
         blizzardWind = loadSound("blizzardWind");
-    }
 }
-
+        private static Sound loadSound(String soundName){
+            if(!Vars.headless){
+                String name = "sounds/" + soundName;
+                String path = Vars.tree.get(name + ".ogg").exists() ? name + ".ogg" : name + ".mp3";
+                
+                Sound sound = new Sound();
+                
+                AssetDescriptor<?> desc = Core.assets.load(path, Sound.class, new SoundLoader.SoundParameter(sound));
+                desc.errored = Throwable::printStackTrace;
+                return sound;
+            }else return new Sound();
+        }
+    }
 
