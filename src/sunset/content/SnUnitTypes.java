d83b926354f9af222f184612b2082bc3fcffb4aa
Slish <81778048+Slish563@users.noreply.github.com>
2021-06-29 11:57:53 +0300

    Update SnUnitTypes.java

--- src/sunset/content/SnUnitTypes.java
+++ src/sunset/content/SnUnitTypes.java
@@ -1,24 +1,21 @@
-package sunset.content; 
+package sunset.content;
 
-import arc.graphics.*;
-import arc.graphics.g2d.*;
-import arc.math.*;
-import arc.struct.*;
-import mindustry.ai.types.*;
-import mindustry.ctype.*;
-import mindustry.content.*;
-import mindustry.entities.*;
-import mindustry.entities.abilities.*;
-import mindustry.entities.bullet.*;
-import mindustry.entities.effect.*;
-import mindustry.gen.*;
-import mindustry.graphics.*;
-import mindustry.type.*;
-import mindustry.world.meta.*;
-import sunset.ai.weapon.ExtinguishWeaponAI;
-import sunset.content.*;
+import mindustry.Vars;
+import mindustry.content.UnitTypes;
+import mindustry.ctype.Content;
+import mindustry.ctype.ContentList;
+import mindustry.entities.bullet.BasicBulletType;
+import mindustry.game.Team;
+import mindustry.gen.Sounds;
+import mindustry.gen.UnitEntity;
+import mindustry.type.UnitType;
+import mindustry.type.Weapon;
 import sunset.ai.ExtinguishAI;
+import sunset.ai.FlyingUnitWeaponAI;
+import sunset.ai.weapon.EmptyWeaponAI;
+import sunset.ai.weapon.ExtinguishWeaponAI;
 import sunset.type.AutoWeapon;
+import sunset.type.ChainWeapon;
 import sunset.type.CopterUnitType;
 import sunset.type.LiquidWeapon;
 
@@ -26,7 +23,7 @@ import static arc.graphics.g2d.Draw.*;
 import static arc.graphics.g2d.Lines.*;
 import static arc.math.Angles.*;
 import static mindustry.Vars.*;
-
+import sunset.type.UnitTypeExt;
 public class SnUnitTypes implements ContentList{
    public static UnitType
 
