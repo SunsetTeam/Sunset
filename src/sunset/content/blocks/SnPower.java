fdbf39171f9a4f927f781377b16ce534cc5a345a
Slish <81778048+Slish563@users.noreply.github.com>
2021-07-04 13:05:49 +0300

    Power

--- src/sunset/content/blocks/SnPower.java
+++ src/sunset/content/blocks/SnPower.java
@@ -12,17 +12,39 @@ import mindustry.gen.Sounds;
 import mindustry.type.Category;
 import mindustry.world.Block;
 import mindustry.world.blocks.power.NuclearReactor;
+import mindustry.world.blocks.power.ItemLiquidGenerator;
+import mindustry.world.blocks.power.DecayGenerator;
 import sunset.content.SnItems;
 
 public class SnPower implements ContentList{
     public static Block
 
 //reactors
-    planatriumReactor;
+    oilGenerator, advrtgGenerator, planatriumReactor;
 
     @Override
     public void load() {
+    
+    oilGenerator = new ItemLiquidGenerator("oil-generator"){{
+            requirements(Category.power, with(Items.copper, 110, Items.titanium, 70, Items.lead, 120, Items.silicon, 55, Items.metaglass, 70));
+            powerProduction = 18f;
+            itemDuration = 220f;
+            hasLiquids = true;
+            hasItems = false;
+            size = 3;
+            ambientSound = Sounds.steam;
+            ambientSoundVolume = 0.03f;
 
+            consumes.liquid(Liquids.oil, 0.18f);
+        }};
+
+    advrtgGenerator = new DecayGenerator("advance-rtg-generator"){{
+            requirements(Category.power, with(Items.lead, 240, Items.silicon, 185, Items.phaseFabric, 75, Items.plastanium, 125, Items.thorium, 130, SnItems.planatrium, 100));
+            size = 4;
+            powerProduction = 9.5f;
+            itemDuration = 55 * 12f;
+        }};
+     
     planatriumReactor = new NuclearReactor("planatrium-reactor"){{
             requirements(Category.power, with(Items.lead, 400, Items.silicon, 270, Items.graphite, 220, SnItems.planatrium, 200, SnItems.fors, 180, SnItems.nobium, 120));
             ambientSound = Sounds.hum;
