026f9249de8acb3ff74b2b1d8e4e321632f85cb6
KirillAldashkin <a89086520092k@yandex.ru>
2021-09-07 23:59:01 +0800

    Advanced thermal generator

--- src/sunset/content/blocks/SnPower.java
+++ src/sunset/content/blocks/SnPower.java
@@ -2,6 +2,7 @@ package sunset.content.blocks;
 
 import static mindustry.type.ItemStack.with;
 
+import mindustry.content.Fx;
 import mindustry.content.Items;
 import mindustry.content.Liquids;
 import mindustry.ctype.ContentList;
@@ -12,17 +13,18 @@ import mindustry.world.blocks.power.NuclearReactor;
 import mindustry.world.blocks.power.DecayGenerator;
 import sunset.world.blocks.power.LiquidGenerator;
 import sunset.content.SnItems;
+import sunset.world.blocks.power.ThermalGeneratorExt;
 
 public class SnPower implements ContentList{
     public static Block
 
 //reactors
-    oilGenerator, advrtgGenerator, planatriumReactor;
+    oilGenerator, advrtgGenerator, planatriumReactor, advThermalGenerator;
 
     @Override
     public void load() {
-    
-    oilGenerator = new LiquidGenerator("oil-generator"){{
+
+        oilGenerator = new LiquidGenerator("oil-generator"){{
             requirements(Category.power, with(Items.copper, 110, Items.titanium, 70, Items.lead, 120, Items.silicon, 55, Items.metaglass, 70));
             powerProduction = 8.3f;
             itemDuration = 220f;
@@ -36,16 +38,16 @@ public class SnPower implements ContentList{
             ambientSoundVolume = 0.03f;
 
             consumes.liquid(Liquids.oil, 0.18f);
-        }};
+            }};
 
-    advrtgGenerator = new DecayGenerator("advance-rtg-generator"){{
+        advrtgGenerator = new DecayGenerator("advance-rtg-generator"){{
             requirements(Category.power, with(Items.lead, 240, Items.silicon, 185, Items.phaseFabric, 75, Items.plastanium, 125, Items.thorium, 130, SnItems.planatrium, 100));
             size = 4;
             powerProduction = 13.2f;
             itemDuration = 55 * 12f;
         }};
-     
-    planatriumReactor = new NuclearReactor("planatrium-reactor"){{
+
+        planatriumReactor = new NuclearReactor("planatrium-reactor"){{
             requirements(Category.power, with(Items.lead, 400, Items.silicon, 270, Items.graphite, 220, SnItems.planatrium, 200, SnItems.fors, 180, SnItems.nobium, 120));
             ambientSound = Sounds.hum;
             ambientSoundVolume = 0.24f;
@@ -57,10 +59,22 @@ public class SnPower implements ContentList{
             liquidCapacity = 80;
             itemDuration = 280f;
             powerProduction = 38f;
-//latter            explodeEffect = Fx.reactorExplosion;
+    //latter            explodeEffect = Fx.reactorExplosion;
             consumes.item(SnItems.planatrium);
             heating = 0.07f;
             consumes.liquid(Liquids.cryofluid, heating / coolantPower).update(false);
         }};
-}
+
+        advThermalGenerator = new ThermalGeneratorExt("advancedThermalGenerator"){{
+            requirements(Category.power, with(Items.copper, 140, Items.graphite, 90, SnItems.naturite, 80, Items.silicon, 40, Items.metaglass, 60));
+            size = 3;
+            powerProduction = 2.6f;
+            attributeScale = 1.2352941f;
+            generateEffect = Fx.redgeneratespark;
+            floating = true;
+            ambientSound = Sounds.hum;
+            ambientSoundVolume = 0.06f;
+            consumes.liquid(Liquids.water, 0.35f);
+        }};
+    }
 }
