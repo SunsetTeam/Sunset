44d1bf8fd7b3ed8666e9de4ce7da12b9757b5e79
KirillAldashkin <a89086520092k@yandex.ru>
2021-09-07 23:23:29 +0800

    Forgot some files

---
+++ src/sunset/world/meta/SnCustomStat.java
@@ -0,0 +1,8 @@
+package sunset.world.meta;
+
+import arc.scene.ui.layout.Table;
+
+/** Интерфейс для контента, который отображает свои параметры самостоятельно. */
+public interface SnCustomStat {
+    void displayStats(Table table);
+}
