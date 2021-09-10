b3be1593957d769d450b7ba59735a492ddab0ee6
Krizis112 <59326567+Krizis112@users.noreply.github.com>
2021-09-10 20:54:48 +0800

    Create BerserkLaserBulletType.java

---
+++ src/sunset/entities/bullet/BerserkLaserBulletType.java
@@ -0,0 +1,26 @@
+package sunset.entities.bullet;
+
+import arc.math.Mathf;
+import arc.util.Log;
+import mindustry.entities.bullet.BasicBulletType;
+import mindustry.gen.Bullet;
+
+import java.awt.*;
+import java.lang.reflect.Array;
+
+public class BerserkLaserBulletType extends BasicBulletType {
+    public Color[] colors = new Color[]{Color.black, Color.blue, Color.white};
+
+    @Override
+    public void hit(Bullet b, float x, float y) {
+        if(b.data == null) {
+            float lenght = Mathf.dst(b.x, b.y, x, y);
+            b.data = lenght;
+        }
+    }
+
+    @Override
+    public void update(Bullet b) {
+        if(b.data != null) Log.info((float)b.data);
+    }
+}
