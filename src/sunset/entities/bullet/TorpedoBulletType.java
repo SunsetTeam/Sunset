e3ceea781a05a592c829cc285466d4213b7e1dc4
Krizis112 <59326567+Krizis112@users.noreply.github.com>
2021-09-10 17:37:40 +0800

    Не то скинул

--- src/sunset/entities/bullet/TorpedoBulletType.java
+++ src/sunset/entities/bullet/TorpedoBulletType.java
@@ -1,19 +1,12 @@
 package sunset.entities.bullet;
 
-
-import arc.graphics.Color;
 import mindustry.Vars;
-import mindustry.entities.bullet.BasicBulletType;
+import mindustry.entities.bullet.BulletType;
 import mindustry.gen.Bullet;
-import mindustry.graphics.Trail;
 
-public class TorpedoBulletType extends BasicBulletType {
-	public void update(Bullet b) {
-		if(!Vars.world.tileWorld(b.x, b.y).floor().isLiquid) b.remove();
-			if(b.data == null) b.data = new Trail(15);
-			if(b.data == null)((Trail)b.data).update(b.x, b.y);
-	}
-	public void draw(Bullet b) {
-		((Trail)b.data).draw(Color.lightGray, 15);
-	}
+public class TorpedoBulletType extends BulletType {
+    @Override
+    public void update(Bullet b) {
+        if(!Vars.world.tileWorld(b.x, b.y).floor().isLiquid) b.remove();
+    }
 }
