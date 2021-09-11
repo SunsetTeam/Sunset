4462dd03077c536f7ce6aaf84a384168172a0051
Slish <81778048+Slish563@users.noreply.github.com>
2021-09-11 10:16:28 +0300

    Buffers hitSize, fix

--- src/sunset/content/SnUnitTypes.java
+++ src/sunset/content/SnUnitTypes.java
@@ -265,7 +265,7 @@ public class SnUnitTypes implements ContentList {
         // region buffers
         comet = new UnitType("comet") {{
             health = 150;
-            hitSize = 12;
+            hitSize = 15;
             speed = 3.1f;
             accel = 0.15f;
             drag = 0.1f;
@@ -297,7 +297,7 @@ public class SnUnitTypes implements ContentList {
 
         satelite = new UnitTypeExt("satellite") {{
             health = 470;
-            hitSize = 16;
+            hitSize = 23;
             speed = 3f;
             accel = 0.2f;
             drag = 0.15f;
@@ -327,7 +327,7 @@ public class SnUnitTypes implements ContentList {
 
         planet = new UnitTypeExt("planet") {{
             health = 980;
-            hitSize = 20;
+            hitSize = 26;
             speed = 2.9f;
             accel = 0.25f;
             drag = 0.1f;
@@ -359,7 +359,7 @@ public class SnUnitTypes implements ContentList {
 
         star = new UnitTypeExt("star") {{
             health = 5800;
-            hitSize = 28;
+            hitSize = 40;
             speed = 2.7f;
             accel = 0.2f;
             drag = 0.1f;
@@ -409,7 +409,7 @@ public class SnUnitTypes implements ContentList {
 
         galaxy = new UnitTypeExt("galaxy") {{
             health = 20000;
-            hitSize = 42;
+            hitSize = 60;
             speed = 2.4f;
             accel = 0.1f;
             drag = 0.05f;
