31183427e4d336596f4d6ae68ff6fea329d4bfbe
Slish <81778048+Slish563@users.noreply.github.com>
2021-09-12 17:41:58 +0300

    Nice

--- src/sunset/entities/abilities/BerserkAbility.java
+++ src/sunset/entities/abilities/BerserkAbility.java
@@ -5,7 +5,6 @@ import mindustry.entities.abilities.Ability;
 public class BerserkAbility {
     public float needHealth, damageMultiplier, speedMultiplier;
 
-
     public static BerserkAbility dmg(float needHealth, float damageMultiplier, float speedMultiplier){
         BerserkAbility ba = new BerserkAbility();
         ba.needHealth = needHealth;
