a020787009e6515aa3dc0b514cbb03c455d60ba1
Krizis112 <59326567+Krizis112@users.noreply.github.com>
2021-09-14 18:31:07 +0800

    Update BerserkLaserBulletType.java

--- src/sunset/entities/bullet/BerserkLaserBulletType.java
+++ src/sunset/entities/bullet/BerserkLaserBulletType.java
@@ -1,23 +1,25 @@
 package sunset.entities.bullet;
 
-import arc.Core;
 import arc.graphics.*;
 import arc.graphics.g2d.*;
 import arc.math.*;
 import arc.math.geom.*;
+import arc.struct.Seq;
 import arc.util.*;
 import mindustry.content.*;
 import mindustry.entities.*;
 import mindustry.entities.bullet.*;
 import mindustry.gen.*;
-import mindustry.graphics.Drawf;
+import sunset.entities.abilities.BerserkAbility;
 
 
 public class BerserkLaserBulletType extends BulletType {
     public float maxLaserLength = 120f;
     public float width = 3;
+    private float swidth = 1;
     public Effect hitEffect = Fx.none;
-    public Color color = Color.gray;
+    public Seq<BerserkAbility> bw = new Seq<>();
+    public Color[] colors = new Color[]{Color.yellow, Color.blue};
 
 
     @Override
@@ -54,9 +56,22 @@ public class BerserkLaserBulletType extends BulletType {
             Position data = (Position)((Object[])b.data)[0];
             Tmp.v1.set(data).lerp(b, b.fin());
             float dst = (float)((Object[])b.data)[1];
-            Draw.color(color);
-            Drawf.laser(b.team, Core.atlas.find("laser"), Core.atlas.find("laser-end"), b.x, b.y, Tmp.v1.x, Tmp.v1.y, 3);
-            Drawf.light(b.team, b.x, b.y, Tmp.v1.x, Tmp.v1.y,15, lightColor, lightOpacity);
+            for (Color color : colors){
+                Draw.color(color);
+                Lines.stroke(width * swidth);
+                Lines.lineAngle(b.x, b.y, b.rotation(), dst);
+            }
         }
+        for(int i = 0; i>bw.size; i++){
+            if(b.owner instanceof Unit){
+                Unit u = (Unit) b.owner;
+                if(u.health < bw.get(i).needHealth){
+                    this.swidth = bw.get(i).damageMultiplier;
+                }
+            }
+        }
+    }
+    public void setWidth(BerserkAbility... berserkAbilities){
+        this.bw = Seq.with(berserkAbilities);
     }
 }
