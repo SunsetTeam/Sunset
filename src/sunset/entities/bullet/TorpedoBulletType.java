d883f448d59b77af4a361cd3eab4b9ecdbc48515
Slish <90574933+Slish562@users.noreply.github.com>
2021-09-16 22:22:35 +0300

    Update TorpedoBulletType.java

--- src/sunset/entities/bullet/TorpedoBulletType.java
+++ src/sunset/entities/bullet/TorpedoBulletType.java
@@ -4,7 +4,7 @@ import mindustry.Vars;
 import mindustry.entities.bullet.BulletType;
 import mindustry.gen.Bullet;
 
-public class TorpedoBulletType extends BulletType {
+public class TorpedoBulletType extends BasicBulletType {
     @Override
     public void update(Bullet b) {
         if(!Vars.world.tileWorld(b.x, b.y).floor().isLiquid) b.remove();
