1c2a3dfb73e3184284e8e4f79df2b39fa9d610f1
Slish <90574933+Slish562@users.noreply.github.com>
2021-09-16 22:38:00 +0300

    Update TorpedoBulletType.java

--- src/sunset/entities/bullet/TorpedoBulletType.java
+++ src/sunset/entities/bullet/TorpedoBulletType.java
@@ -3,8 +3,16 @@ package sunset.entities.bullet;
 import mindustry.Vars;
 import mindustry.entities.bullet.BulletType;
 import mindustry.gen.Bullet;
+import sunset.content.*;
 
 public class TorpedoBulletType extends BasicBulletType {
+public ArtilleryLiquidBulletType(float speed, float damage) {
+        super(speed, damage);
+        layer = Layer.debris + 0.001f;
+        collidesAir = absorbable = keepVelocity = false
+        trailEffect = SnFx.torpedoTrail;
+    }
+
     @Override
     public void update(Bullet b) {
         if(!Vars.world.tileWorld(b.x, b.y).floor().isLiquid) b.remove();
