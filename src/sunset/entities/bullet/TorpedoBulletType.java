d3e2a378b96f160e861713e1d5476522dee069dd
Slish <90574933+Slish562@users.noreply.github.com>
2021-09-17 19:34:19 +0300

    TorpedoBullet layer

--- src/sunset/entities/bullet/TorpedoBulletType.java
+++ src/sunset/entities/bullet/TorpedoBulletType.java
@@ -1,22 +1,25 @@
 package sunset.entities.bullet;
 
 import mindustry.Vars;
-import mindustry.entities.*;
 import mindustry.entities.bullet.*;
-import mindustry.entities.effect.*;
-import mindustry.entities.bullet.BulletType;
-import mindustry.gen.*;
-import mindustry.graphics.*;
+import arc.graphics.g2d.Draw;
 import mindustry.graphics.Layer;
 import mindustry.gen.Bullet;
 import sunset.content.*;
 
 public class TorpedoBulletType extends BasicBulletType {
-public TorpedoBulletType (float speed, float damage) {
+    public TorpedoBulletType (float speed, float damage) {
         super(speed, damage);
         collidesAir = absorbable = keepVelocity = false;
         trailEffect = SnFx.torpedoTrail;
     }
+    @Override
+    public void draw(Bullet b){
+        Draw.z(Layer.debris);
+        super.draw(b);
+}
+
+
 
     @Override
     public void update(Bullet b) {
