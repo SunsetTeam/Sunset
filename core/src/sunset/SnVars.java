b2b43430f2c5efa63ae54665a6e057b957aea49f
Zelaux <ura20060605@gmail.com>
2021-10-22 00:10:24 +0300

    Content list sorted

--- core/src/sunset/SnVars.java
+++ core/src/sunset/SnVars.java
@@ -1,75 +1,84 @@
 package sunset;
 
-import arc.struct.*;
-import mindustry.*;
-import mindustry.ctype.*;
-import mma.*;
+import arc.struct.Seq;
+import mindustry.Vars;
+import mindustry.ctype.ContentList;
+import mma.ModVars;
 import sunset.content.*;
 import sunset.content.blocks.*;
-import sunset.content.blocks.defense.*;
+import sunset.content.blocks.defense.SnProjectors;
+import sunset.content.blocks.defense.SnTurrets;
+import sunset.content.blocks.defense.SnWalls;
 //import sunset.utils.V7.SunsetWaveSpawner;
 
-public class SnVars extends ModVars{
+public class SnVars extends ModVars {
 
     //public static SunsetWaveSpawner spawner;
 
-    public static final int maxSchematicSize = 128;
     private static final Seq<Runnable> onLoad = new Seq<>();
     private static final ContentList[] snContent = {
-    new SnStatusEffects(),
-    new SnLiquids(),
-    new SnItems(),
-    new SnBullets(),
-    new SnUnitTypes(),
-    new SnTurrets(),
-    new SnEnvironment(),
-    new SnProjectors(),
-    new SnWalls(),
-    new SnDistribution(),
-    new SnPower(),
-    new SnProduction(),
-    new SnWeathers(),
-    new SnPlanets(),
-    new SnLiquidBlocks(),
-    new SnLogic(),
-    new SnUnitBlocks(),
-    new SnRaw(),
-    new SnSectorPresets()
+            new SnItems(),
+            new SnStatusEffects(),
+            new SnLiquids(),
+            new SnBullets(),
+            new SnUnitTypes(),
+            //blocks region
+            new SnProjectors(),
+            new SnTurrets(),
+            new SnWalls(),
+
+            new SnDistribution(),
+            new SnEnvironment(),
+            new SnLiquidBlocks(),
+            new SnLogic(),
+            new SnPower(),
+            new SnProduction(),
+            new SnRaw(),
+            new SnUnitBlocks(),
+            //region end
+            //new SnLoadouts(),
+            new SnWeathers(),
+            new SnPlanets(),
+            new SnSectorPresets(),
+            new SnTechTree(),
     };
 
-    static{
+    static {
         new SnVars();
     }
-    /**just void method*/
-    public static void create(){
+
+    /**
+     * just void method
+     */
+    public static void create() {
 
     }
 
-    public static void init(){
+    public static void init() {
 
     }
 
-    public static void load(){
+    public static void load() {
 
     }
 
     @Override
-    protected void onLoad(Runnable runnable){
+    protected void onLoad(Runnable runnable) {
         onLoad.add(runnable);
     }
 
     @Override
-    protected void showException(Throwable ex){
+    protected void showException(Throwable ex) {
         Vars.ui.showException(ex);
     }
 
     @Override
-    public ContentList[] getContentList(){
+    public ContentList[] getContentList() {
         return snContent;
     }
 
     @Override
-    public String getFullName(String name){
+    public String getFullName(String name) {
         return null;
     }
 }
---
+++ core/src/sunset/content/SnTechTree.java
@@ -0,0 +1,11 @@
+package sunset.content;
+
+import mindustry.ctype.ContentList;
+import mma.type.TechTreeManager;
+
+public class SnTechTree extends TechTreeManager implements ContentList {
+    @Override
+    public void load() {
+
+    }
+}
