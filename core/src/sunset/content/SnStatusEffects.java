ef28b726ab1c6667910ec9e2f8fa0ac111ebcbc3
Zelaux <ura20060605@gmail.com>
2021-10-22 17:31:25 +0300

    Improved display of overheating effects.

--- core/src/sunset/content/SnStatusEffects.java
+++ core/src/sunset/content/SnStatusEffects.java
@@ -1,22 +1,30 @@
 package sunset.content;
 
-import arc.graphics.*;
-import arc.graphics.g2d.*;
-import mindustry.content.*;
-import mindustry.ctype.*;
-import mindustry.gen.*;
-import mindustry.graphics.*;
-import mindustry.type.*;
-import sunset.type.*;
+import arc.graphics.Color;
+import arc.graphics.g2d.Draw;
+import arc.graphics.g2d.TextureRegion;
+import arc.graphics.gl.FrameBuffer;
+import mindustry.Vars;
+import mindustry.content.Fx;
+import mindustry.content.StatusEffects;
+import mindustry.ctype.ContentList;
+import mindustry.gen.Unit;
+import mindustry.graphics.Layer;
+import mindustry.graphics.Pal;
+import mindustry.graphics.Shaders;
+import mindustry.type.StatusEffect;
+import sunset.type.StackableStatusEffect;
 
-public class SnStatusEffects implements ContentList{
+import static arc.Core.camera;
+
+public class SnStatusEffects implements ContentList {
     public static StatusEffect
-    frostbite, stun, starBuff, galaxyDebuff, overheat, electricalShort, reloading, viscous;
+            frostbite, stun, starBuff, galaxyDebuff, overheat, electricalShort, reloading, viscous;
 
     @Override
-    public void load(){
+    public void load() {
 
-        frostbite = new StatusEffect("frostbite"){{
+        frostbite = new StatusEffect("frostbite") {{
             color = Color.valueOf("6ecdec");
             damage = 0.17f;
             speedMultiplier = 0.4f;
@@ -33,13 +41,13 @@ public class SnStatusEffects implements ContentList{
             });
         }};
 
-        stun = new StatusEffect("stun"){{
+        stun = new StatusEffect("stun") {{
             color = Color.valueOf("392f17");
             speedMultiplier = 0f;
             disarm = true;
         }};
 
-        starBuff = new StatusEffect("star-buff"){{
+        starBuff = new StatusEffect("star-buff") {{
             color = Pal.surge;
             speedMultiplier = 1.3f;
             healthMultiplier = 1.15f;
@@ -47,41 +55,55 @@ public class SnStatusEffects implements ContentList{
             reloadMultiplier = 1.1f;
         }};
 
-        galaxyDebuff = new StatusEffect("galaxy-debuff"){{
+        galaxyDebuff = new StatusEffect("galaxy-debuff") {{
             speedMultiplier = 0.8f;
             damageMultiplier = 0.9f;
             reloadMultiplier = 0.8f;
         }};
 
-        overheat = new StackableStatusEffect("overheat"){
+        overheat = new StackableStatusEffect("overheat") {
+            boolean draw = false;
+
             {
                 color = Color.valueOf("FF30000");
                 maxStacks = 20;
                 healthMultipliers.addAll(0.99f, 0.98f, 0.97f, 0.96f, 0.95f, 0.94f, //-0.01
-                0.92f, 0.90f, 0.86f, 0.84f, 0.82f, //-0.02
-                0.79f, 0.76f, 0.73f, 0.70f, //-0.03
-                0.65f, 0.60f, 0.55f, //-0.05
-                0.47f, 0.39f); //-0.08
+                        0.92f, 0.90f, 0.86f, 0.84f, 0.82f, //-0.02
+                        0.79f, 0.76f, 0.73f, 0.70f, //-0.03
+                        0.65f, 0.60f, 0.55f, //-0.05
+                        0.47f, 0.39f); //-0.08
                 speedMultipliers.addAll(0.99f, 0.99f, 0.99f, 0.99f, 0.98f,
-                0.98f, 0.98f, 0.98f, 0.97f, 0.96f,
-                0.96f, 0.96f, 0.95f, 0.94f, 0.93f,
-                0.93f, 0.93f, 0.92f, 0.91f, 0.90f);
+                        0.98f, 0.98f, 0.98f, 0.97f, 0.96f,
+                        0.96f, 0.96f, 0.95f, 0.94f, 0.93f,
+                        0.93f, 0.93f, 0.92f, 0.91f, 0.90f);
                 healthMultiplier = 0.99f;
                 speedMultiplier = 0.99f;
                 color = Color.red;
             }
 
             @Override
-            public void drawStack(Unit unit, int stackCount){
+            public void drawStack(Unit unit, int stackCount) {
+                if (draw) return;
+                Vars.renderer.effectBuffer.begin();
+
+                draw = true;
+                unit.draw();
+                draw=false;
+
+                Vars.renderer.effectBuffer.end();
+
                 Draw.z((unit.isFlying() ? Layer.flyingUnit : Layer.groundUnit) + 1);
-                TextureRegion text = unit.type.region;
+
                 Draw.color(color, (stackCount / 2f) / maxStacks);
-                Draw.rect(text, unit.x, unit.y, unit.rotation - 90f);
+                TextureRegion wrap = Draw.wrap(Vars.renderer.effectBuffer.getTexture());
+                wrap.flip(false,true);
+                Draw.rect(wrap, camera.position.x, camera.position.y, camera.width, camera.height);
+//                Vars.renderer.effectBuffer.blit(Shaders.screenspace);
                 Draw.color();
             }
         };
 
-        electricalShort = new StatusEffect("electric-short"){{
+        electricalShort = new StatusEffect("electric-short") {{
             effectChance = 1;
             speedMultiplier = 0;
             disarm = true;
@@ -101,13 +123,13 @@ public class SnStatusEffects implements ContentList{
             });
         }};
 
-        reloading = new StatusEffect("reboot"){{
+        reloading = new StatusEffect("reboot") {{
             speedMultiplier = 0;
             disarm = true;
             color = Color.valueOf("047070");
         }};
 
-        viscous = new StatusEffect("viscous"){{
+        viscous = new StatusEffect("viscous") {{
             color = Color.valueOf("721A1A");
             speedMultiplier = 0.94f;
             effect = SnFx.viscous;
--- core/src/sunset/world/meta/SnStatCat.java
+++ core/src/sunset/world/meta/SnStatCat.java
@@ -3,6 +3,5 @@ package sunset.world.meta;
 import acontent.world.meta.AStatCat;
 
 public class SnStatCat {
-    public static final AStatCat
-            durability=AStatCat.get("durability");
+    public static final AStatCat durability=AStatCat.get("durability");
 }
