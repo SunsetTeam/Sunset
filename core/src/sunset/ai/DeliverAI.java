083e60048cee75f83262aa37bb3a1be58f73a49d
Zelaux <ura20060605@gmail.com>
2021-10-24 19:12:17 +0300

    Added saving of airport configuration and server compatibility.

--- core/src/sunset/ai/DeliverAI.java
+++ core/src/sunset/ai/DeliverAI.java
@@ -1,6 +1,7 @@
 package sunset.ai;
 
 import arc.math.geom.Vec2;
+import mindustry.Vars;
 import mindustry.ai.types.FlyingAI;
 import sunset.world.blocks.units.Airport;
 
@@ -8,6 +9,7 @@ public class DeliverAI extends FlyingAI {
     public boolean inited = false;
     public Airport.AirportBuild base;
     public int state = 0;
+
     // 0 - move to "base"
     // 1 - pick resources from "base"
     // 2 - move to "base.linked"
@@ -16,57 +18,59 @@ public class DeliverAI extends FlyingAI {
         this.base = base;
         inited = true;
     }
+
     @Override
     public void updateMovement() {
-        if(!inited) return;
-        if(base == null || !base.added) {
+        if (!inited) return;
+        if (base == null || !base.isValid()) {
             unit.killed();
-        } else {
-            if(state == 0) {
-                float len = new Vec2(unit.x, unit.y).sub(base.x, base.y).len();
-                if(len > unit.hitSize * 2) {
-                    moveTo(new Vec2(base.x, base.y), unit.hitSize);
-                } else {
-                    state = 1;
-                }
-            } else if(state == 1) {
-                if(unit.stack.amount > 0 && (unit.stack.item != base.takeItem)) {
-                    state = 2;
-                } else {
-                    if(base.takeItem != null) {
-                        int need = unit.itemCapacity() - unit.stack.amount;
-                        if (need > 0) {
-                            int toMove = Math.min(need, base.items.get(base.takeItem));
-                            base.items.remove(base.takeItem, toMove);
-                            unit.addItem(base.takeItem, toMove);
-                        } else {
-                            state = 2;
-                        }
-                    }
-                }
-            } else if(state == 2) {
-                if(base.linked != null) {
-                    float len = new Vec2(unit.x, unit.y).sub(base.linked.x, base.linked.y).len();
-                    if(len > unit.hitSize * 2) {
-                        moveTo(new Vec2(base.linked.x, base.linked.y), unit.hitSize);
+            return;
+        }
+        Airport.AirportBuild linked = base.link != -1 && Vars.world.build(base.link) instanceof Airport.AirportBuild build && build.isValid() ? build : null;
+        if (state == 0) {
+            float len = new Vec2(unit.x, unit.y).sub(base.x, base.y).len();
+            if (len > unit.hitSize * 2) {
+                moveTo(new Vec2(base.x, base.y), unit.hitSize);
+            } else {
+                state = 1;
+            }
+        } else if (state == 1) {
+            if (unit.stack.amount > 0 && (unit.stack.item != base.takeItem)) {
+                state = 2;
+            } else {
+                if (base.takeItem != null) {
+                    int need = unit.itemCapacity() - unit.stack.amount;
+                    if (need > 0) {
+                        int toMove = Math.min(need, base.items.get(base.takeItem));
+                        base.items.remove(base.takeItem, toMove);
+                        unit.addItem(base.takeItem, toMove);
                     } else {
-                        state = 3;
+                        state = 2;
                     }
                 }
-            } else if(state == 3) {
-                if(base.linked != null) {
-                    if(unit.stack.amount > 0) {
-                        int remains = base.linked.getMaximumAccepted(unit.stack.item) -
-                                base.linked.items.get(unit.stack.item);
-                        int toMove = Math.min(unit.stack.amount, remains);
-                        base.linked.items.add(unit.stack.item, toMove);
-                        unit.stack.amount -= toMove;
-                    } else {
-                        state = 0;
-                    }
+            }
+        } else if (state == 2) {
+            if (linked != null) {
+                float len = new Vec2(unit.x, unit.y).sub(linked.x, linked.y).len();
+                if (len > unit.hitSize * 2) {
+                    moveTo(new Vec2(linked.x, linked.y), unit.hitSize);
                 } else {
-                    state = 2;
+                    state = 3;
                 }
+            }
+        } else if (state == 3) {
+            if (linked != null) {
+                if (unit.stack.amount > 0) {
+                    int remains = linked.getMaximumAccepted(unit.stack.item) -
+                            linked.items.get(unit.stack.item);
+                    int toMove = Math.min(unit.stack.amount, remains);
+                    linked.items.add(unit.stack.item, toMove);
+                    unit.stack.amount -= toMove;
+                } else {
+                    state = 0;
+                }
+            } else {
+                state = 2;
             }
         }
     }
--- core/src/sunset/world/blocks/units/Airport.java
+++ core/src/sunset/world/blocks/units/Airport.java
@@ -4,24 +4,31 @@ import arc.Core;
 import arc.graphics.g2d.Draw;
 import arc.graphics.g2d.Lines;
 import arc.math.Mathf;
+import arc.math.geom.Point2;
+import arc.math.geom.Vec2;
 import arc.scene.ui.layout.Table;
-import arc.struct.FloatSeq;
 import arc.struct.IntSeq;
+import arc.struct.Seq;
+import arc.util.Pack;
 import arc.util.Time;
 import arc.util.io.Reads;
 import arc.util.io.Writes;
 import mindustry.entities.Units;
-import mindustry.gen.*;
+import mindustry.gen.Building;
+import mindustry.gen.Groups;
+import mindustry.gen.Unit;
 import mindustry.graphics.Drawf;
 import mindustry.graphics.Pal;
+import mindustry.io.TypeIO;
 import mindustry.type.Item;
 import mindustry.type.ItemStack;
 import mindustry.type.UnitType;
 import mindustry.ui.Bar;
 import mindustry.world.blocks.ItemSelection;
 import mindustry.world.blocks.storage.StorageBlock;
-import mindustry.world.meta.*;
-import sunset.utils.Utils;
+import mindustry.world.meta.Stat;
+import mindustry.world.meta.StatUnit;
+import mindustry.world.meta.StatValues;
 import sunset.ai.DeliverAI;
 import sunset.content.SnUnitTypes;
 import sunset.world.consumers.AdjustableConsumePower;
@@ -39,14 +46,25 @@ public class Airport extends StorageBlock {
         super(name);
         update = true;
         configurable = true;
+        this.<Item, AirportBuild>config(Item.class, (tile, item) -> {
+            tile.takeItem = item;
+        });
+        this.<Integer, AirportBuild>config(Integer.class, (tile, link) -> {
+            tile.link = link;
+        });
+        this.<Long, AirportBuild>config(Long.class, (tile, link) -> {
+            tile.link = Point2.unpack(Pack.leftInt(link)).add(tile.tile.x,tile.tile.y).pack();
+            int id = Pack.rightInt(link);
+            tile.takeItem = id==-1?null:content.item(id);
+        });
     }
 
     @Override
-    public void init(){
+    public void init() {
         super.init();
 
         consumes.add(new AdjustableConsumePower(powerUse, b -> {
-            AirportBuild ab = (AirportBuild)b;
+            AirportBuild ab = (AirportBuild) b;
             return ab.shouldBuild ? ab.getBoost() : 0f;
         }));
     }
@@ -60,18 +78,18 @@ public class Airport extends StorageBlock {
     public void setBars() {
         super.setBars();
         bars.add("progress", (AirportBuild e) ->
-            new Bar("bar.progress", Pal.ammo, () -> e.construcionTime / unitBuildTime));
+                new Bar("bar.progress", Pal.ammo, () -> e.construcionTime / unitBuildTime));
 
         bars.add("units", (AirportBuild e) ->
-            new Bar(
-                () -> Core.bundle.format("bar.unitcap",
-                    "TODO: string",
-                    e.units.size,
-                    maxUnitCount
-                ),
-                () -> Pal.power,
-                () -> (float)e.units.size / maxUnitCount
-            ));
+                new Bar(
+                        () -> Core.bundle.format("bar.unitcap",
+                                "TODO: string",
+                                e.units.size,
+                                maxUnitCount
+                        ),
+                        () -> Pal.power,
+                        () -> (float) e.units.size / maxUnitCount
+                ));
     }
 
     @Override
@@ -85,90 +103,87 @@ public class Airport extends StorageBlock {
     }
 
     public class AirportBuild extends StorageBuild {
-        public int linkID = 0;
-        public AirportBuild linked;
-        public float construcionTime = 0f;
-        public boolean readen = false;
         final IntSeq units = new IntSeq();
+        final Seq<Vec2> positions = new Seq<>();
+        public int link = 0;
+        public float construcionTime = 0f;
+        public boolean afterRead = false;
         public boolean shouldBuild;
         public Item takeItem;
+        IntSeq states = new IntSeq();
+        private long boostEndTime = 0;
+        private float boost = 0f;
 
-        private void validateLink(){
-            AirportBuild prevLinked = linked;
-            Building b = world.build(linkID);
-            if(b instanceof AirportBuild) {
-                linked = (AirportBuild)b;
-                if(linked.dead) linked = null;
-            } else linked = null;
-            if(linked != prevLinked) Call.tileConfig(player, this, linked);
-        }
         @Override
         public void updateTile() {
-            if(readen) firstUpdateAfterRead();
-            validateLink();
-            for (int i = units.size-1; i > -1; i--) {
-                Unit unit = Groups.unit.getByID(units.get(i));
-                if (unit == null || unit.dead()) units.removeIndex(i);
+            if (afterRead) firstUpdateAfterRead();
+            if (link == -1) return;
+            Building linkBuild = world.build(link);
+            if (!(linkBuild instanceof AirportBuild linked) || !linked.isValid()) {
+//                configure(-1);
+                return;
             }
-            shouldBuild = items.has(requirements) && units.size < maxUnitCount && linked != null;
+            for (int i = 0; i < units.size; i++) {
+                Unit unit = Groups.unit.getByID(units.get(i));
+                if (unit == null || unit.dead() || !unit.isValid()) {
+                    units.removeIndex(i);
+                    i--;
+                }
+            }
+            shouldBuild = items.has(requirements) && units.size < maxUnitCount;
             if (shouldBuild) {
                 construcionTime += Time.delta * getBoost() * efficiency();
                 if (construcionTime > unitBuildTime) {
                     items.remove(requirements);
                     construcionTime = 0;
                     Unit u = SnUnitTypes.courier.spawn(team, x, y); //TODO починить тип юнита
-                    ((DeliverAI)u.controller()).setup(this);
+                    ((DeliverAI) u.controller()).setup(this);
                     units.add(u.id);
                 }
             }
         }
 
-        private long boostEndTime = 0;
-        private float boost = 0f;
-        public float getBoost(){
-            return Time.millis()<=boostEndTime ? Math.max(boost, 1) : 1;
+        public float getBoost() {
+            return Time.millis() <= boostEndTime ? Math.max(boost, 1) : 1;
         }
+
         @Override
         public void applyBoost(float intensity, float duration) {
-            boostEndTime = Time.millis() + (long)(duration*1000f/60f);
+            boostEndTime = Time.millis() + (long) (duration * 1000f / 60f);
             boost = intensity;
         }
 
         @Override
-        public void buildConfiguration(Table table){
+        public void buildConfiguration(Table table) {
             ItemSelection.buildTable(table, content.items(), () -> takeItem, this::configure);
         }
 
         @Override
-        public void configure(Object value) {
-            takeItem = (Item)value;
-            Call.tileConfig(player, this, value);
+        public Long config() {
+            return Pack.longInt(Point2.unpack(link).sub(tile.x,tile.y).pack(),takeItem==null?-1:takeItem.id);
         }
 
         @Override
         public boolean onConfigureTileTapped(Building other) {
-            if (this == other || linked == other) {
-                linked = null;
-                linkID = 0;
-                Call.tileConfig(player, this, linked);
+            if (this == other || (other != null && link == other.pos())) {
+                configure(-1);
                 return false;
             } else if (other instanceof AirportBuild) {
-                linkID = other.pos();
-                validateLink();
+                configure(other.pos());
                 return false;
             }
             return true;
         }
 
         @Override
-        public void drawConfigure(){
+        public void drawConfigure() {
             float sin = Mathf.absin(Time.time, 6f, 1f);
 
             Draw.color(Pal.accent);
             Lines.stroke(1f);
             Drawf.circles(x, y, (tile.block().size / 2f + 1) * tilesize + sin - 2f, Pal.accent);
 
-            if(linked != null){
+            if (link != -1 && world.build(link) instanceof AirportBuild linked && linked.isValid()) {
                 Drawf.circles(linked.x, linked.y, (linked.block().size / 2f + 1) * tilesize + sin - 2f, Pal.place);
                 Drawf.arrow(x, y, linked.x, linked.y, size * tilesize + sin, 4f + sin);
             }
@@ -178,50 +193,45 @@ public class Airport extends StorageBlock {
         public void write(Writes write) {
             super.write(write);
             write.s(takeItem == null ? -1 : takeItem.id);
-            write.i(linkID);
+            write.i(link);
             write.f(construcionTime);
             write.i(units.size);
             for (int i = 0; i < units.size; i++) {
                 Unit u = Groups.unit.getByID(units.get(i));
                 write.f(u.x);
                 write.f(u.y);
-                write.i(((DeliverAI)u.controller()).state);
+                write.i(((DeliverAI) u.controller()).state);
             }
         }
 
-        IntSeq states = new IntSeq();
-        FloatSeq xs = new FloatSeq();
-        FloatSeq ys = new FloatSeq();
         @Override
         public void read(Reads read, byte revision) {
             super.read(read, revision);
             takeItem = content.item(read.s());
-            linkID = read.i();
+            link = read.i();
             construcionTime = read.f();
             states.clear();
-            xs.clear();
-            ys.clear();
             int unitsCount = read.i();
             for (int i = 0; i < unitsCount; i++) {
-                xs.add(read.f());
-                ys.add(read.f());
+                positions.add(TypeIO.readVec2(read));
                 states.add(read.i());
             }
-            readen = true;
+            afterRead = true;
         }
+
         public void firstUpdateAfterRead() {
             units.clear();
             for (int i = 0; i < states.size; i++) {
-                Unit u = Units.closest(null,xs.get(i), ys.get(i), (_u) -> true);
-                DeliverAI ai = (DeliverAI)u.controller();
+                Vec2 position = positions.get(i);
+                Unit u = Units.closest(null, position.x, position.y, (_u) -> true);
+                DeliverAI ai = (DeliverAI) u.controller();
                 ai.setup(this);
                 ai.state = states.get(i);
                 units.add(u.id);
             }
             states.clear();
-            xs.clear();
-            ys.clear();
-            readen = false;
+            positions.clear();
+            afterRead = false;
         }
     }
 }
