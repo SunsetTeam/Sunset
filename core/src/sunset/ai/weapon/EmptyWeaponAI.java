73481c7168c46c87f61fe3ace184e2c99656db80
KirillAldashkin <a89086520092k@yandex.ru>
2021-12-11 15:54:12 +0800

    A lot of fixes: Weapon AI

--- core/src/sunset/ai/weapon/EmptyWeaponAI.java
+++ core/src/sunset/ai/weapon/EmptyWeaponAI.java
@@ -1,7 +1,10 @@
 package sunset.ai.weapon;
 
+import mindustry.entities.units.WeaponMount;
+import mindustry.gen.Unit;
+
 /** AI, которое ничего не делает с орудием. Может быть полезно в специфичных случаях. */
 public class EmptyWeaponAI extends WeaponAI {
     @Override
-    public void update() { }
+    public boolean update(Unit unit, WeaponMount mount) { return false; }
 }
