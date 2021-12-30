6877e7490811652434783253194a148d199a113d
Slish562 <dendeniska2019@gmail.com>
2021-12-30 19:30:11 +0200

    layerOffset fix

--- core/src/sunset/content/SnUnitTypes.java
+++ core/src/sunset/content/SnUnitTypes.java
@@ -206,7 +206,7 @@ public class SnUnitTypes implements ContentList {
                         mirror = true;
                         x = 11f;
                         y = 10f;
-                        layerOffset = -10f;
+                        layerOffset = -1f;
                         spacing = 3f;
                         reload = 40f;
                         shake = 1f;
@@ -285,7 +285,6 @@ public class SnUnitTypes implements ContentList {
                         shake = 2f;
                         x = -21f;
                         y = -1f;
-                        layerOffset = -10f;
                         reload = 20f;
                         shotDelay = 1f;
                         shootY = 3;
@@ -299,7 +298,7 @@ public class SnUnitTypes implements ContentList {
                         mirror = true;
                         x = 17f;
                         y = 17f;
-                        layerOffset = -10f;
+                        layerOffset = -1f;
                         spacing = 4;
                         reload = 40f;
                         recoil = 5f;
@@ -316,7 +315,7 @@ public class SnUnitTypes implements ContentList {
                         shake = 3f;
                         x = 0f;
                         y = 18f;
-                        layerOffset = -10f;
+                        layerOffset = -1f;
                         reload = 37f;
                         shotDelay = 4f;
                         shots = 5;
