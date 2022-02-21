10878ee6f248b822ac568679e52758c61dbf25fc
Slish <90574933+Slish562@users.noreply.github.com>
2022-02-21 21:48:29 +0200

    Aymirus turrets fixes #3

--- core/src/sunset/entities/bullet/AimBulletType.java
+++ core/src/sunset/entities/bullet/AimBulletType.java
@@ -13,29 +13,24 @@ import mindustry.world.blocks.defense.turrets.Turret.TurretBuild;
 import sunset.utils.Utils.Targeting;
 
 public class AimBulletType extends BasicBulletType {
-    public boolean homing = true;
-    public BulletType bullet;
 
     public AimBulletType(float speed, float damage){
         super(speed, damage);
     }
 
-
-    public void targetPos(Bullet bullet) {
-        if (homing) {
-            if (bullet.fdata() != 1 && bullet.collided.size < 2) {
-                Tmp.v1.set(bullet.x, bullet.y);
-                if (bullet.owner instanceof TurretBuild) {
-                    Tmp.v1.set(((TurretBuild) bullet.owner).targetPos.x, ((TurretBuild) bullet.owner).targetPos.y);
-                } else if (bullet.owner instanceof Unitc) {
-                    Tmp.v1.set(((Unitc) bullet.owner).aimX(), ((Unitc) bullet.owner).aimY());
-                } else if (bullet.owner instanceof Targeting) {
-                    Tmp.v1.set(((Targeting) bullet.owner).targetPos());
-                }
-                bullet.vel.setAngle(Angles.moveToward(bullet.rotation(), bullet.angleTo(Tmp.v1.x, Tmp.v1.y), Time.delta * 261f * bullet.fin()));
-                if (bullet.within(Tmp.v1.x, Tmp.v1.y, bullet.hitSize)) {
-                    bullet.fdata = 1;
-                }
+    public void targetPosition(Bullet bullet) {
+        if (bullet.fdata() != 1 && bullet.collided.size < 2) {
+            Tmp.v1.set(bullet.x, bullet.y);
+            if (bullet.owner instanceof TurretBuild) {
+                Tmp.v1.set(((TurretBuild) bullet.owner).targetPos.x, ((TurretBuild) bullet.owner).targetPos.y);
+            } else if (bullet.owner instanceof Unitc) {
+                Tmp.v1.set(((Unitc) bullet.owner).aimX(), ((Unitc) bullet.owner).aimY());
+            } else if (bullet.owner instanceof Targeting) {
+                Tmp.v1.set(((Targeting) bullet.owner).targetPos());
+            }
+            bullet.vel.setAngle(Angles.moveToward(bullet.rotation(), bullet.angleTo(Tmp.v1.x, Tmp.v1.y), Time.delta * 260f * bullet.fin()));
+            if (bullet.within(Tmp.v1.x, Tmp.v1.y, bullet.hitSize)) {
+                bullet.fdata = 1;
             }
         }
     }
