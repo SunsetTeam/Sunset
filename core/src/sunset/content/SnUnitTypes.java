9c378dad803966096a5bd60d8efe849f82b73fad
xStaBUx <96493687+xStaBUx@users.noreply.github.com>
2022-03-26 16:48:27 +0200

    hover test

--- core/src/sunset/content/SnUnitTypes.java
+++ core/src/sunset/content/SnUnitTypes.java
@@ -25,6 +25,7 @@ import mindustry.type.Weapon;
 import mindustry.type.ammo.ItemAmmoType;
 import mindustry.type.ammo.PowerAmmoType;
 import mindustry.type.weapons.PointDefenseWeapon;
+import mindustry.world.meta.BlockFlag;
 import sunset.ai.*;
 import sunset.ai.weapon.ExtinguishWeaponAI;
 import sunset.content.blocks.SnCrafting;
@@ -47,6 +48,8 @@ import sunset.type.weapons.ChainWeapon;
 import sunset.type.weapons.SnWeapon;
 import sunset.type.weapons.WeaponExt;
 import sunset.utils.UnitsUtils;
+
+import static mindustry.Vars.tilePayload;
 import static sunset.utils.UnitsUtils.addUnitGroup;
 import static mindustry.type.ItemStack.with;
 
@@ -65,7 +68,7 @@ public class SnUnitTypes implements ContentList{
     wheelT1, wheelT2, wheelT3, wheelT4, wheelT5, wheelT6,
     freezingT1,
     //air
-    engineT1,
+    engineT1, hoverTest,
     //hylight
     light,
     //naval
@@ -1727,6 +1730,79 @@ public class SnUnitTypes implements ContentList{
                         engineSize = 2f;
                     }});
         }};
+
+        hoverTest = new HoverUnitType("hover-test"){{
+            armor = 8f;
+            health = 6000;
+            speed = 1.2f;
+            rotateSpeed = 2f;
+            accel = 0.05f;
+            drag = 0.017f;
+            lowAltitude = false;
+            flying = true;
+            circleTarget = true;
+            engineOffset = 12f;
+            engineSize = 6f;
+            rotateShooting = false;
+            hitSize = 36f;
+            payloadCapacity = (3 * 3) * tilePayload;
+            buildSpeed = 2.5f;
+            buildBeamOffset = 23;
+            range = 140f;
+            targetAir = false;
+            targetFlags = new BlockFlag[]{BlockFlag.battery, BlockFlag.factory, null};
+            hoverBloomColor = Pal.darkestGray;
+
+            ammoType = new PowerAmmoType(3000);
+
+            weapons.add(
+                    new Weapon(){{
+                        x = y = 0f;
+                        mirror = false;
+                        reload = 55f;
+                        minShootVelocity = 0.01f;
+
+                        soundPitchMin = 1f;
+                        shootSound = Sounds.plasmadrop;
+
+                        bullet = new BasicBulletType(){{
+                            sprite = "large-bomb";
+                            width = height = 120/4f;
+
+                            maxRange = 30f;
+                            ignoreRotation = true;
+
+                            backColor = Pal.darkestGray;
+                            frontColor = Pal.darkestGray;
+                            mixColorTo = Pal.darkestGray;
+
+                            hitSound = Sounds.boom;
+
+                            shootCone = 280f;
+                            ejectEffect = Fx.hitFlamePlasma;
+                            hitShake = 5f;
+
+                            collidesAir = false;
+
+                            lifetime = 85f;
+
+                            despawnEffect = Fx.instBomb;
+                            hitEffect = Fx.massiveExplosion;
+                            keepVelocity = true;
+                            spin = 2f;
+
+                            shrinkX = shrinkY = 0.7f;
+
+                            speed = 0f;
+                            collides = false;
+
+                            healPercent = 15f;
+                            splashDamage = 220f;
+                            splashDamageRadius = 80f;
+                        }};
+                    }});
+        }};
+
         //endregion other
         //region hylight
         light = new EngineUnitType("light") {{
@@ -1768,16 +1844,15 @@ public class SnUnitTypes implements ContentList{
             engines(
                     new Engine("small-engine") {{
                         underUnit = true;
-                        engineX = -6.4f;
-                        engineY = 3.4f;
+                        engineX = -6.2f;
+                        engineY = 3.2f;
                         engineSize = 1.8f;
-                        innerOffsetY = 6f;
                     }},
 
                     new Engine("small-engine") {{
                         underUnit = true;
-                        engineX = 6.4f;
-                        engineY = 3.4f;
+                        engineX = 6.2f;
+                        engineY = 3.2f;
                         engineSize = 1.8f;
                     }},
 
