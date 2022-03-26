ea0339d6a11665c85bdadc3636a8115c9b305bf6
Slish <90574933+Slish562@users.noreply.github.com>
2022-03-26 16:04:08 +0200

    HoverUnitType #2

--- core/src/sunset/type/unitTypes/HoverUnitType.java
+++ core/src/sunset/type/unitTypes/HoverUnitType.java
@@ -1,15 +1,20 @@
 package sunset.type.unitTypes;
 
 import arc.Core;
+import arc.graphics.Blending;
+import arc.graphics.Color;
 import arc.graphics.g2d.Draw;
 import arc.graphics.g2d.TextureRegion;
 import arc.math.Mathf;
 import mindustry.gen.Unit;
 import mindustry.gen.UnitEntity;
 import mindustry.graphics.Layer;
+import mindustry.graphics.Pal;
 
 public class HoverUnitType extends UnitTypeExt{
     public boolean drawHover = true;
+    public Color hoverBloomColor = Pal.engine;
+    public float alpha = 0.5f;
 
     public TextureRegion hoverRegion;
 
@@ -25,6 +30,7 @@ public class HoverUnitType extends UnitTypeExt{
     }
 
     public void draw(Unit unit){
+        super.draw(unit);
         float z = unit.elevation > 0.1f ? (lowAltitude ? Layer.flyingUnitLow : Layer.flyingUnit) : groundLayer + Mathf.clamp(hitSize / 4000f, 0, 0.01f);
         Draw.z(z);
         if(drawHover) drawHover(unit);
@@ -32,6 +38,14 @@ public class HoverUnitType extends UnitTypeExt{
 
     public void drawHover(Unit unit){
         Draw.rect(hoverRegion, unit.x, unit.y, unit.rotation - 90);
+        if(Core.settings.getBool("bloom")){
+            Draw.rect(hoverRegion, unit.x, unit.y, unit.rotation);
+        } else {
+            Draw.blend(Blending.additive);
+            Draw.color(hoverBloomColor, alpha);
+            Draw.rect(hoverRegion, unit.x, unit.y, unit.rotation);
+            Draw.blend();
+        }
         Draw.reset();
     }
 }
