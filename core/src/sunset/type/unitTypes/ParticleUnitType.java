8b2a7bb22b4791e0630c323d4c5c48ae56820544
Slish <90574933+Slish562@users.noreply.github.com>
2022-04-09 13:26:33 +0300

    ops

--- core/src/sunset/type/unitTypes/ParticleUnitType.java
+++ core/src/sunset/type/unitTypes/ParticleUnitType.java
@@ -5,7 +5,7 @@ import mindustry.entities.effect.ParticleEffect;
 import mindustry.gen.UnitEntity;
 
 public class ParticleUnitType extends UnitTypeExt{
-=    public Seq<ParticleEffect> particleEffects = new Seq<>();
+    public Seq<ParticleEffect> particleEffects = new Seq<>();
 
     public ParticleUnitType(String name){
         super(name);
