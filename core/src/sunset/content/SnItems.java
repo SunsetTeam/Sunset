beeca13401c63f2f3858b58046b8be5d10511b7e
Slish <90574933+Slish562@users.noreply.github.com>
2022-05-05 11:37:48 +0300

    gradlew pack | some fixes

--- core/src/sunset/content/SnItems.java
+++ core/src/sunset/content/SnItems.java
@@ -7,9 +7,12 @@ import sunset.type.Ammo;
 
 public class SnItems implements ContentList {
     public static Item
-    //items
-    //azaria
-    fors, erius, anzar, naturite, nedirium, reneubite, nobium, planatrium, yobut, yantai, enojie, zerinium,
+    // region azaria
+    //raw
+    fors, erius, nedirium, planatrium,
+    //crafting
+    anzar, naturite,  reneubite, nobium,  yobut, yantai, enojie, zerinium,
+    //endregion azaria
     //other
     flameid, coldent,
     lightBurstMissile,
@@ -18,19 +21,19 @@ public class SnItems implements ContentList {
 
     @Override
     public void load() {
-        //azaria
+        //region azaria
         fors = new Item("fors", Color.valueOf("ed606e")) {{
-            cost = 3f;
+            cost = 2f;
             hardness = 3;
         }};
         erius = new Item("erius", Color.valueOf("a0d2d6")) {{
-            cost = 3f;
+            cost = 2f;
         }};
         anzar = new Item("anzar", Color.valueOf("feb380")) {{
             cost = 3f;
         }};
         naturite = new Item("naturite", Color.valueOf("f3e979")) {{
-            cost = 3f;
+            cost = 2f;
         }};
         nedirium = new Item("nedirium", Color.valueOf("ffffff")) {{
             cost = 3f;
@@ -39,29 +42,42 @@ public class SnItems implements ContentList {
             cost = 3f;
         }};
         nobium = new Item("nobium", Color.valueOf("c1c4cb")) {{
-            cost = 3f;
+            cost = 5f;
         }};
         planatrium = new Item("planatrium", Color.valueOf("b287ec")) {{
-            cost = 3f;
+            cost = 5f;
+            hardness = 4;
+            explosiveness = 0.2f;
+            radioactivity = 1f;
         }};
         yobut = new Item("yobut", Color.valueOf("ff0000")) {{
-            cost = 3f;
+            cost = 4f;
+            flammability = 0.6f;
         }};
         yantai = new Item("yantai", Color.valueOf("fbff93")) {{
-            cost = 3f;
+            cost = 5f;
         }};
         enojie = new Item("enojie", Color.valueOf("99f2c7")) {{
-            cost = 3f;
+            cost = 6f;
+            radioactivity = 0.8f;
+            explosiveness = 0.01f;
         }};
         zerinium = new Item("zerinium", Color.valueOf("ffffff")) {{
-            cost = 3f;
+            cost = 5f;
         }};
+        //endregion azaria
+
+        //region other
         flameid = new Item("flameid", Color.valueOf("ffffff")) {{
-            cost = 3f;
+            cost = 4f;
+            flammability = 0.6f;
+            hardness = 4;
         }};
         coldent = new Item("coldent", Color.valueOf("ffffff")) {{
-           cost = 3f;
+            cost = 4f;
+            hardness = 4;
         }};
+        //endregion other
         // test
         /*
 
