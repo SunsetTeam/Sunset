cccb2925ae2ccc682f08e812f00efe6ddfb99b0a
Slish <90574933+Slish562@users.noreply.github.com>
2022-05-13 11:07:55 +0300

    Some crafting fixes/changes

--- core/src/sunset/content/blocks/SnCrafting.java
+++ core/src/sunset/content/blocks/SnCrafting.java
@@ -1,6 +1,7 @@
 package sunset.content.blocks;
 
 import arc.graphics.Color;
+import gas.world.consumers.ConsumeGas;
 import mindustry.content.Fx;
 import mindustry.content.Items;
 import mindustry.content.Liquids;
@@ -18,8 +19,10 @@ import mindustry.world.draw.DrawRotator;
 import mindustry.world.draw.DrawSmelter;
 import mma.world.draw.MultiDrawSmelter;
 import sunset.content.SnFx;
+import sunset.content.SnGas;
 import sunset.content.SnItems;
 import sunset.content.SnLiquids;
+import sunset.world.blocks.gas.GasCrafter;
 import sunset.world.draw.DrawAngleRotator;
 import sunset.world.draw.DrawModWeave;
 import sunset.world.draw.DrawSurge;
@@ -139,27 +142,8 @@ public class SnCrafting implements ContentList{
         }};
         //endregion advanced
         //region standard
-        collider = new GenericCrafter("collider"){{
-            requirements(Category.crafting, with(Items.copper, 105, SnItems.fors, 100, Items.silicon, 65));
-
-            outputItem = new ItemStack(SnItems.reneubite, 3);
-            //craftEffect = SnFx.;
-            //updateEffect = SnFx.;
-            craftTime = 46f;
-            size = 3;
-            itemCapacity = 15;
-            liquidCapacity = 30f;
-            hasPower = true;
-            hasLiquids = true;
-            ambientSound = Sounds.grinding;
-            ambientSoundVolume = 0.025f;
-
-            consumes.items(with(Items.pyratite, 3, Items.graphite, 2));
-            consumes.liquid(SnLiquids.burheyna, 0.4f);
-            consumes.power(3.5f);
-        }};
         crystallizer = new GenericCrafter("crystallizer"){{
-            requirements(Category.crafting, with(Items.copper, 100, Items.lead, 130, Items.metaglass, 110, Items.silicon, 120));
+            requirements(Category.crafting, with(SnItems.fors, 100, SnItems.erius, 130));
 
             craftEffect = SnFx.crystalyze;
             updateEffect = SnFx.crystalyzeSmall;
@@ -167,23 +151,22 @@ public class SnCrafting implements ContentList{
             itemCapacity = 30;
             liquidCapacity = 30f;
             outputItem = new ItemStack(SnItems.naturite, 2);
-            craftTime = 34f;
+            craftTime = 43f;
             size = 3;
             hasPower = true;
             drawer = new DrawSmelter(){{
                 flameColor = Color.valueOf("F9ECA3");
             }};
 
-            consumes.item(Items.sand, 3);
-            consumes.liquid(SnLiquids.burheyna, 0.17f);
-            consumes.power(2f);
+            consumes.liquid(SnLiquids.burheyna, 0.40f);
+            consumes.power(3.2f);
         }};
         anzarKiln = new GenericCrafter("anzar-kiln"){{
-            requirements(Category.crafting, with(Items.copper, 120, Items.silicon, 70, SnItems.fors, 100, SnItems.naturite, 60));
+            requirements(Category.crafting, with(SnItems.fors, 80, SnItems.naturite, 60, SnItems.erius, 40));
 
             hasItems = true;
             itemCapacity = 30;
-            outputItem = new ItemStack(SnItems.naturite, 2);
+            outputItem = new ItemStack(SnItems.anzar, 1);
             craftTime = 40f;
             size = 2;
             hasPower = true;
@@ -192,10 +175,11 @@ public class SnCrafting implements ContentList{
             }};
 
             consumes.items(with(SnItems.fors, 2, SnItems.naturite, 1));
-            consumes.power(3f);
+            consumes.power(2.3f);
         }};
+
         purifier = new GenericCrafter("purifier"){{
-            requirements(Category.crafting, with(Items.copper, 110, SnItems.fors, 95, Items.silicon, 80, SnItems.anzar, 65));
+            requirements(Category.crafting, with(SnItems.fors, 110, SnItems.erius, 95, SnItems.naturite, 80, SnItems.anzar, 65));
 
             outputItem = new ItemStack(SnItems.nobium, 1);
             craftEffect = Fx.pulverize;
@@ -209,22 +193,45 @@ public class SnCrafting implements ContentList{
             ambientSound = Sounds.grinding;
             ambientSoundVolume = 0.025f;
 
-            consumes.items(with(SnItems.anzar, 3, SnItems.planatrium, 1, SnItems.fors, 1));
-            consumes.power(2.5f);
+            consumes.items(with(SnItems.fors, 2, SnItems.nedirium, 1, SnItems.erius, 1));
+            consumes.power(4.3f);
         }};
-        enojieKiln = new GenericCrafter("enojie-kiln"){{
-            requirements(Category.crafting, with(Items.lead, 180, SnItems.nobium, 150, Items.graphite, 140, Items.silicon, 120, SnItems.fors, 80));
 
-            outputItem = new ItemStack(SnItems.enojie, 2);
-            craftTime = 69f;
+        collider = new GasCrafter("collider"){{
+            requirements(Category.crafting, with(SnItems.fors, 105, SnItems.erius, 100, SnItems.anzar, 65));
+
+            outputItem = new ItemStack(SnItems.reneubite, 3);
+            //craftEffect = SnFx.;
+            //updateEffect = SnFx.;
+            craftTime = 55f;
+            size = 3;
+            itemCapacity = 15;
+            liquidCapacity = 40f;
+            gasCapacity = 40f;
+            hasPower = true;
+            hasLiquids = true;
+            ambientSound = Sounds.grinding;
+            ambientSoundVolume = 0.025f;
+
+            consumes.item(SnItems.nedirium, 2);
+            consumes.addGas(new ConsumeGas(SnGas.hyneris, 0.7f));
+            consumes.liquid(SnLiquids.burheyna, 0.8f);
+            consumes.power(4.6f);
+        }};
+
+        enojieKiln = new GenericCrafter("enojie-kiln"){{
+            requirements(Category.crafting, with(SnItems.erius, 180, SnItems.nobium, 150, SnItems.naturite, 140, SnItems.anzar, 120));
+
+            outputItem = new ItemStack(SnItems.enojie, 1);
+            craftTime = 58f;
             size = 4;
             hasPower = true;
             hasLiquids = false;
             craftEffect = SnFx.enojieCraft;
             updateEffect = SnFx.enojieBurn;
 
-            consumes.items(with(SnItems.nobium, 1, SnItems.planatrium, 3, Items.metaglass, 1));
-            consumes.power(5.3f);
+            consumes.items(with(SnItems.nobium, 1, SnItems.planatrium, 2, SnItems.erius, 1));
+            consumes.power(6f);
         }};
         //endregion standard
         //region missile
